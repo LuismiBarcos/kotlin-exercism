72cb6347283eab44ec276919d42a789b97a83660
Luismi Barcos <luismiguel.barcos@liferay.com>
2023-08-02 15:10:04 +0200

    Add some initial comments to the actual code.

--- src/main/java/com/exercism/jalber786/squeaky/clean/SqueakyClean.java
+++ src/main/java/com/exercism/jalber786/squeaky/clean/SqueakyClean.java
@@ -17,7 +17,12 @@ class SqueakyClean {
                     i++;
                 }
             } else if ((isntLetter(currChar)) || ((Character.toString(currChar)).matches("[α-ω]"))) {
-
+                /*
+                 * An empty if used to be a code smell as it add complexity to do nothing.
+                 * Some options that you have here could be or rethink the condition to make it the last else
+                 * or, if you consider this is condition is pretty clear, use the `continue` sentence to follow to the
+                 * next for iteration
+                 */
             } else {
                 stringBuilder.append(currChar);
             }
@@ -26,7 +31,27 @@ class SqueakyClean {
         return stringBuilder.toString();
     }
 
+    /*
+    I like this method as you are extracting a complex validation to a specific method and the name of the method
+    is helping to know what we are doing.
+    Just a comment, the convention used to be naming the methods starting with is or isNot, so in this case could be
+    `isNotALetter`
+     */
     private static boolean isntLetter(char c) {
+
+        /*
+            You are using matches method, which returns a boolean. If mathes return true, you leave the value variable
+            to true. If returns false, you change the value variable to false, so, basically, you are storing the return
+            of the matches method.
+
+            This practice could end in unnecessary code, making necessary more lines, more variables, more processing...
+            Something you could do is
+            boolean value = (Character.toString(c)).matches("[^\\p{L}\\p{P}\\p{Z}]");
+            return value;
+
+            Or, even cleaner, return just the matches result
+            return (Character.toString(c)).matches("[^\\p{L}\\p{P}\\p{Z}]");
+         */
 
         boolean value = false;
         if ((Character.toString(c)).matches("[^\\p{L}\\p{P}\\p{Z}]")) {
